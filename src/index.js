9aebd569304fb444647d8a8f4dedafeb665694b0
矢野大樹 <daikitech0123@gmail.com>
2019-09-15 18:48:38 -0700

    Add css-loader and scss-loader

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/styles.scss';
 
 class Header extends React.Component {
     render() {
       return (
         <div>
-          <h2>Treact-Flaskテスト</h2>
+          <h2>TEST</h2>
         </div>
       );
     }
   }
-
-
-class Index extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2>react-Flask</h2>
-      </div>
-    );
-  }
-}
+// class Index extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h2>react-Flask</h2>
+//       </div>
+//     );
+//   }
+// }
 ReactDOM.render(
 <div>
-<Index/>
+
 <Header/>
 </div>
   ,
